01233ca94aa9c6f988d0aa18eeab1e698f93343a
hamasato0000 <>
2025-10-23 18:26:36 +0900

    add: アップロード用のURLを発行する仮APIを実装

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,37 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const app = new Hono();
 
+const s3 = new S3Client({
+    region: process.env.AWS_REGION!,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+});
+
 app.get("/", (c) => c.text("Hello Hono 🫶"));
+
+app.post("/contents/generate-signed-url", async (c) => {
+    console.log(c);
+    const objectKey = "test.text"; // TODO: 適切なオブジェクトキーを設定
+    const mime = "text/plain"; // TODO: 適切なMIMEタイプを設定
+    const putObjectCommand = new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET!,
+        Key: objectKey,
+        ContentType: mime,
+    });
+
+    // アップロード用の署名付きURLを生成
+    const uploadUrl = await getSignedUrl(s3, putObjectCommand, {
+        expiresIn: 60, // 60秒
+    });
+
+    return c.json({ uploadUrl });
+});
 
 const port = Number(process.env.PORT ?? 3000);
 console.log(`Listening on http://localhost:${port}`);
