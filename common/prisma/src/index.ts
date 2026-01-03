1f48a3064d150d721986f45817c00825d4d3403f
hamasato0000 <hamasato48@gmail.com>
2026-01-04 00:25:20 +0900

    update: コメント追加・修正

--- common/prisma/src/index.ts
+++ common/prisma/src/index.ts
@@ -1,16 +1,17 @@
-// common/prisma/src/index.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
     prisma?: PrismaClient;
 };
 
+// PrismaClientのインスタンスを作成
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
         // log: ['query', 'info', 'warn', 'error'], // 必要なら
     });
 
+// 開発環境ではグローバルにキャッシュしてホットリロード時の再生成を防止
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
